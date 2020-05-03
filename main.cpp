29d5ed59da06df601b4ff11e1598a88801b6c9a4
empty-works <pama.mark@gmail.com>
2020-05-03 22:49:41 +0200

    Added skeleton code to main.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+
+struct Cities {
+
+	
+};
+
+struct Countries {
+
+
+};
+
+struct Tours {
+
+
+};
+
+int main() {
+
+	return 0;
+}
