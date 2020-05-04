d5b1b838436c24dca1e3d8c058b112aa687c31fc
empty-works <pama.mark@gmail.com>
2020-05-04 20:54:25 +0200

    Added a ruler to measure the positions of each character. Also instantiated a Tour object with Country and City objects.

--- main.cpp
+++ main.cpp
@@ -24,5 +24,40 @@ struct Tour {
 
 int main() {
 
+	std::string ruler {"1234567890123456789012345678901234567890123456789012345678901234567890"};
+	std::cout << ruler << std::endl;
+
+	Tour cool_tours
+	{"Tour Ticket Prices from San Francisco", 
+		{
+			{
+				"South Korea",
+				{
+					{"Seoul", 8797979, 400.98},
+					{"Busan", 3495295, 424.12},
+					{"Incheon", 2898374, 415.54},
+					{"Daegu", 2459698, 425.23}
+				}
+			},
+			{
+				"Japan",
+				{
+					{"Tokyo", 8635903, 528.22},
+					{"Yokohama", 3648592, 450.25},
+					{"Osaka", 2689489, 350.85},
+					{"Nagoya", 2285938, 439.02}
+				}
+			},
+			{
+				"Philippines",
+				{
+					{"Quezon City", 2849284, 439.68},
+					{"Manila", 1758939, 429.22},
+					{"Davao City", 1698385, 358.86},
+					{"Caloocan", 1583948, 421.99}
+				}
+			}			
+		}
+	};	
 	return 0;
 }
